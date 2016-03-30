4318d98b06abae57ab29bf8db3a70b9d8046028a
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-30 11:27:45 +0800

    call anywhere

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cat config/list.txt | php bin/generate.php  > config/satis.json
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cat $DIR/../config/list.txt | php $DIR/generate.php  > $DIR/../config/satis.json
